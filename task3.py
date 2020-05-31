94d85783b6914a042df4373b34c5142fe9b8a41e
sourav-cpu <64347351+sourav-cpu@users.noreply.github.com>
2020-05-31 11:21:27 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+df.pd.read_csv("student_math.csv")
+from sklearn preprocessing import labelEncoder
+labelencoder = Labelencoder()
+x[:,9]=labelencoder.fit.transform(x[:,9])
+df["final_grade"]=df["G1"]+df["G2"]+df["G3"]
+y=np.asarray(df["final grade"]
+x[:,10]=labelencoder.fit.transform(x[:,10])
+x[:,11]=labelencoder.fit.transform(x[:,11])
+x[:,12]=labelencoder.fit.transform(x[:,12])
+x=df["school"],df["sex"],df["age"],df["address"],df["famsize"],df["pstatus"],df["medu"],df["fedu"],df["mjob"],df["fjob"],df["reason"],df["guardian"],df["school"],df["school"],df["school"],df["school"],df["school"],df["school"]
